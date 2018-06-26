e6689e8c45e21d3e5333dee1515af31cb5e2a198
William Powell <william.powell92@gmail.com>
2018-06-26 16:28:55 +0100

    Keyboarder sets keyState.Arrowleft to true

--- public/src/Keyboarder.js
+++ public/src/Keyboarder.js
@@ -1,3 +1,10 @@
 function Keyboarder() {
-  this.KEYS = { LEFT: 37, RIGHT: 39 }
+  this.KEYS = { LEFT: 37, RIGHT: 39, UP: 38 };
+  this.keyState = {};
+
+  const self = this;
+
+  window.onkeydown = function onKeyDown(e) {
+    self.keyState.ArrowLeft = true;
+  };
 }
--- spec/KeyboarderSpec.js
+++ spec/KeyboarderSpec.js
@@ -1,9 +1,18 @@
 describe('Keyboarder', () => {
   let keyboarder;
+  let keyPressed = null;
 
   beforeEach(() => {
     keyboarder = new Keyboarder();
   });
+
+  function keyPress(key) {
+    const event = new KeyboardEvent('keydown', {
+      view: window,
+      key
+    });
+    window.dispatchEvent(event);
+  }
 
   describe('KEYS', () => {
     it('should have the correct keycode for left', () => {
@@ -12,6 +21,17 @@ describe('Keyboarder', () => {
 
     it('should have the correct keycode for right', () => {
       expect(keyboarder.KEYS.RIGHT).toEqual(39);
+    });
+
+    it('should have the correct keycode for up', () => {
+      expect(keyboarder.KEYS.UP).toEqual(38);
+    });
+  });
+
+  describe('onkeydown', () => {
+    it('sets the keyState of left key to true', () => {
+      keyPress('ArrowLeft');
+      expect(keyboarder.keyState.ArrowLeft).toEqual(true);
     });
   });
 });
