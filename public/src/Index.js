a1dbc19b2d954cdec206b8b7119adf189c02aae8
William Powell <william.powell92@gmail.com>
2018-06-27 10:25:12 +0100

    Corrected linting errors in index.js

--- public/src/Index.js
+++ public/src/Index.js
@@ -1,13 +1,13 @@
-function Index(gameClass = Game) {
+function Index(GameClass = Game) {
   const canvas = document.querySelector('canvas');
   const screen = canvas.getContext('2d');
   const gameSize = { x: canvas.width, y: canvas.height };
-  this.game = new gameClass('context', gameSize);
+  this.game = new GameClass('context', gameSize);
 
-  const self = this
-  const tick = function() {
+  const self = this;
+  const tick = function tick() {
     self.game.update(gameSize);
-  }
+  };
 
   tick();
 }
