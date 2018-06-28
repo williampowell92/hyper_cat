a2e341d7669609c9bc8f6ed3a62ac869376d538e
William Powell <william.powell92@gmail.com>
2018-06-28 14:04:34 +0100

    Player collides with top left, top and top right of platform

--- spec/CollisonSpec.js
+++ spec/CollisonSpec.js
@@ -1,16 +1,18 @@
 describe('Collision', () => {
-  let bodies;
   let collision;
   let platform;
   let player1;
   let player2;
+  let player3;
+  let player4;
 
   beforeEach(() => {
     platform = { center: { x: 150, y: 150 }, size: { x: 100, y: 100 } };
     player1 = { center: { x: 90, y: 150 }, size: { x: 20, y: 20 } };
     player2 = { center: { x: 100, y: 100 }, size: { x: 20, y: 20 } };
-    bodies = [];
-    collision = new Collision(bodies);
+    player3 = { center: { x: 150, y: 100 }, size: { x: 20, y: 20 } };
+    player4 = { center: { x: 200, y: 100 }, size: { x: 20, y: 20 } };
+    collision = new Collision();
   });
 
   describe('isColliding', () => {
@@ -20,6 +22,14 @@ describe('Collision', () => {
 
     it('returns true when player collides top left of body', () => {
       expect(collision.isColliding(player2, platform)).toEqual(true);
+    });
+
+    it('returns true when player collides top of body', () => {
+      expect(collision.isColliding(player3, platform)).toEqual(true);
+    });
+
+    it('returns true when player collides top right of body', () => {
+      expect(collision.isColliding(player4, platform)).toEqual(true);
     });
   });
 });
