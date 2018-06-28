a257b212f6b2937c3d88d38e196c63cfde10a21c
William Powell <william.powell92@gmail.com>
2018-06-28 14:07:04 +0100

    Player does not collide when he is right of the platform

--- public/src/Collision.js
+++ public/src/Collision.js
@@ -6,6 +6,7 @@ Collision.prototype = {
   isColliding(body1, body2) {
     return !(
       body1.center.x + body1.size.x / 2 <= body2.center.x - body2.size.x / 2
+      || body1.center.x - body1.size.x / 2 >= body2.center.x + body2.size.x / 2
     );
   }
 };
--- spec/CollisonSpec.js
+++ spec/CollisonSpec.js
@@ -5,6 +5,7 @@ describe('Collision', () => {
   let player2;
   let player3;
   let player4;
+  let player5;
 
   beforeEach(() => {
     platform = { center: { x: 150, y: 150 }, size: { x: 100, y: 100 } };
@@ -12,6 +13,7 @@ describe('Collision', () => {
     player2 = { center: { x: 100, y: 100 }, size: { x: 20, y: 20 } };
     player3 = { center: { x: 150, y: 100 }, size: { x: 20, y: 20 } };
     player4 = { center: { x: 200, y: 100 }, size: { x: 20, y: 20 } };
+    player5 = { center: { x: 210, y: 150 }, size: { x: 20, y: 20 } };
     collision = new Collision();
   });
 
@@ -30,6 +32,10 @@ describe('Collision', () => {
 
     it('returns true when player collides top right of body', () => {
       expect(collision.isColliding(player4, platform)).toEqual(true);
+    });
+
+    it('returns false when player is to the right of body', () => {
+      expect(collision.isColliding(player5, platform)).toEqual(false);
     });
   });
 });
