8c5bdc0905a4f0cbfefbeacabe97569a7e1d3bc2
William Powell <william.powell92@gmail.com>
2018-06-28 15:00:39 +0100

    Player does not collide when player top is below the body top

--- public/src/Collision.js
+++ public/src/Collision.js
@@ -3,10 +3,12 @@ function Collision() {
 }
 
 Collision.prototype = {
-  isColliding(body1, body2) {
+  isCollidingOnTop(body1, body2) {
     return !(
       body1.center.x + body1.size.x / 2 <= body2.center.x - body2.size.x / 2
       || body1.center.x - body1.size.x / 2 >= body2.center.x + body2.size.x / 2
+      || body1.center.y + body1.size.y / 2 <= body2.center.y - body2.size.y / 2
+      || body1.center.y - body1.size.y / 2 >= body2.center.y - body2.size.y / 2
     );
   }
 };
--- spec/CollisonSpec.js
+++ spec/CollisonSpec.js
@@ -6,36 +6,57 @@ describe('Collision', () => {
   let player3;
   let player4;
   let player5;
+  let player6;
+  let player7;
+  let player8;
 
   beforeEach(() => {
     platform = { center: { x: 150, y: 150 }, size: { x: 100, y: 100 } };
-    player1 = { center: { x: 90, y: 150 }, size: { x: 20, y: 20 } };
-    player2 = { center: { x: 100, y: 100 }, size: { x: 20, y: 20 } };
-    player3 = { center: { x: 150, y: 100 }, size: { x: 20, y: 20 } };
-    player4 = { center: { x: 200, y: 100 }, size: { x: 20, y: 20 } };
-    player5 = { center: { x: 210, y: 150 }, size: { x: 20, y: 20 } };
     collision = new Collision();
   });
 
-  describe('isColliding', () => {
+  describe('isCollidingOnTop', () => {
+    beforeEach(() => {
+      player1 = { center: { x: 90, y: 150 }, size: { x: 20, y: 20 } };
+      player2 = { center: { x: 100, y: 100 }, size: { x: 20, y: 20 } };
+      player3 = { center: { x: 150, y: 100 }, size: { x: 20, y: 20 } };
+      player4 = { center: { x: 200, y: 100 }, size: { x: 20, y: 20 } };
+      player5 = { center: { x: 210, y: 150 }, size: { x: 20, y: 20 } };
+      player6 = { center: { x: 150, y: 90 }, size: { x: 20, y: 20 } };
+      player7 = { center: { x: 150, y: 210 }, size: { x: 20, y: 20 } };
+      player8 = { center: { x: 150, y: 120 }, size: { x: 20, y: 20 } };
+    });
+
     it('returns false when player is to the left of body', () => {
-      expect(collision.isColliding(player1, platform)).toEqual(false);
+      expect(collision.isCollidingOnTop(player1, platform)).toEqual(false);
     });
 
     it('returns true when player collides top left of body', () => {
-      expect(collision.isColliding(player2, platform)).toEqual(true);
+      expect(collision.isCollidingOnTop(player2, platform)).toEqual(true);
     });
 
     it('returns true when player collides top of body', () => {
-      expect(collision.isColliding(player3, platform)).toEqual(true);
+      expect(collision.isCollidingOnTop(player3, platform)).toEqual(true);
     });
 
     it('returns true when player collides top right of body', () => {
-      expect(collision.isColliding(player4, platform)).toEqual(true);
+      expect(collision.isCollidingOnTop(player4, platform)).toEqual(true);
     });
 
     it('returns false when player is to the right of body', () => {
-      expect(collision.isColliding(player5, platform)).toEqual(false);
+      expect(collision.isCollidingOnTop(player5, platform)).toEqual(false);
+    });
+
+    it('returns false when player is above the body', () => {
+      expect(collision.isCollidingOnTop(player6, platform)).toEqual(false);
+    });
+
+    it('returns false when player is below the body', () => {
+      expect(collision.isCollidingOnTop(player7, platform)).toEqual(false);
+    });
+
+    it('returns false when player top is below platform top', () => {
+      expect(collision.isCollidingOnTop(player8, platform)).toEqual(false);
     });
   });
 });
