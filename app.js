efc1b8b03b6e9ea7954d11e809c3e5ca91287fc2
Possed <r0xana.pirvu@yahoo.com>
2018-06-29 16:36:44 +0100

    served index.html on express server

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
+/* eslint no-console: 0 */
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.use('/public', express.static(path.join(__dirname, '/public')));
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, './public/views/index.html')));
 
-app.listen(8000, () => console.log('Example app listening to port 8000!'));
+app.listen(8000, () => console.log('Listening to port 8000!'));
