5389c9f8ab9616b40c68f2db1f59fdf74865fc4a
Revilo1882 <oliverrharris@gmail.com>
2018-06-30 14:28:56 +0100

    Change karma config for travis

--- karma.conf.js
+++ karma.conf.js
@@ -1,16 +1,33 @@
-/* eslint 'func-names': 0 */
+/* eslint func-names: 0 */
 
 module.exports = function (config) {
-  config.set({
+  const configuration = {
     basePath: '',
     frameworks: ['jasmine'],
     files: [
       'public/src/*.js',
       'spec/*Spec.js'
     ],
-    browsers: ['Chrome'],
-    singleRun: true,
+    exclude: [
+    ],
+    preprocessors: { 'public/src/*.js': ['coverage'] },
     reporters: ['spec', 'coverage'],
-    preprocessors: { 'public/src/*.js': ['coverage'] }
-  });
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: false,
+    browsers: ['Chrome'],
+    customLaunchers: {
+      Chrome_travis_ci: {
+        base: 'Chrome',
+        flags: ['--no-sandbox']
+      }
+    },
+    singleRun: true,
+    concurrency: Infinity
+  };
+  if (process.env.TRAVIS) {
+    configuration.browsers = ['Chrome_travis_ci'];
+  }
+  config.set(configuration);
 };
