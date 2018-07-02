7c9b7f3c71237d3b9c0870626368d767bf674000
William Powell <william.powell92@gmail.com>
2018-07-02 11:29:03 +0100

    Animation positionFrame function increments frameX by 90

--- public/src/Animation.js
+++ public/src/Animation.js
@@ -1,3 +1,14 @@
 function Animation() {
   this.currentFrame = 0;
+  this.frameX = 0;
 }
+
+Animation.prototype = {
+  updateFrame() {
+    this.currentFrame = (this.currentFrame + 1) % 10;
+  },
+
+  positionFrame() {
+    this.frameX = (this.frameX + 90);
+  }
+};
--- spec/AnimationSpec.js
+++ spec/AnimationSpec.js
@@ -10,4 +10,29 @@ describe('Animation', () => {
       expect(animation.currentFrame).toEqual(0);
     });
   });
+
+  describe('updateFrame', () => {
+    it('should iterate currentFrame from 1 to 9', () => {
+      for (let i = 1; i < 10; i++) {
+        animation.updateFrame();
+        expect(animation.currentFrame).toEqual(i);
+      }
+    });
+
+    it('reset currentFrame to 0 after 9', () => {
+      for (let i = 1; i < 11; i++) {
+        animation.updateFrame();
+      }
+      expect(animation.currentFrame).toEqual(0);
+    });
+  });
+
+  describe('repositionFrame', () => {
+    it('should iterate frameX from 90 to 810', () => {
+      for (let i = 1; i < 10; i++) {
+        animation.positionFrame();
+        expect(animation.frameX).toEqual(i * 90);
+      }
+    });
+  });
 });
