5c6aa5e9b3d5346e73e264f71432ed0f587811dc
Possed <r0xana.pirvu@yahoo.com>
2018-07-03 15:29:15 +0100

    testing images rendering on platforms

--- public/src/Index.js
+++ public/src/Index.js
@@ -6,10 +6,10 @@ function Index(gameFactory = new GameFactory(), documentObject = document) {
     undefined,
     undefined,
     [
-      new Platform({ x: canvas.width / 2, y: canvas.height - 10 }, { x: canvas.width, y: 30 }),
-      new Platform({ x: canvas.width / 2, y: 650 }, { x: 200, y: 100 }),
-      new Platform({ x: canvas.width / 4, y: 400 }, { x: 200, y: 30 }),
-      new Platform({ x: canvas.width * 3 / 4, y: 400 }, { x: 200, y: 30 })
+      new Platform({ x: canvas.width / 2, y: canvas.height - 10 }, { x: canvas.width, y: 30 }, undefined, 'public/assets/imgs/platform_mid.png'),
+      new Platform({ x: canvas.width / 2, y: 650 }, { x: 200, y: 100 }, undefined, 'public/assets/imgs/platform_mid.png'),
+      new Platform({ x: canvas.width / 4, y: 400 }, { x: 200, y: 30 }, undefined, 'public/assets/imgs/platform_mid.png'),
+      new Platform({ x: canvas.width * 3 / 4, y: 400 }, { x: 200, y: 30 }, undefined, 'public/assets/imgs/platform_mid.png')
     ]
   );
 
