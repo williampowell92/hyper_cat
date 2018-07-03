f85964d6643506536ae576c098facb0d4d049671
Possed <r0xana.pirvu@yahoo.com>
2018-07-03 11:11:38 +0100

    updated Index function and test after adding PlatformFactory function

--- public/src/Index.js
+++ public/src/Index.js
@@ -1,15 +1,18 @@
-function Index(gameFactory = new GameFactory(), documentObject = document) {
+function Index(gameFactory = new GameFactory(), platformFactory = new PlatformFactory(),
+  documentObject = document) {
   const canvas = documentObject.querySelector('canvas');
   const context = canvas.getContext('2d');
   const gameSize = { x: canvas.width, y: canvas.height };
+
   this.game = gameFactory.build(
     undefined,
     undefined,
     [
-      new Platform({ x: canvas.width / 2, y: canvas.height - 10 }, { x: canvas.width, y: 30 }),
-      new Platform({ x: canvas.width / 2, y: 650 }, { x: 200, y: 100 }),
-      new Platform({ x: canvas.width / 4, y: 400 }, { x: 200, y: 30 }),
-      new Platform({ x: canvas.width * 3 / 4, y: 400 }, { x: 200, y: 30 })
+      platformFactory.build({ x: canvas.width / 2, y: canvas.height - 10 },
+        { x: canvas.width, y: 30 }),
+      platformFactory.build({ x: canvas.width / 2, y: 650 }, { x: 200, y: 100 }),
+      platformFactory.build({ x: canvas.width / 4, y: 400 }, { x: 200, y: 30 }),
+      platformFactory.build({ x: canvas.width * 3 / 4, y: 400 }, { x: 200, y: 30 })
     ]
   );
 
--- spec/IndexSpec.js
+++ spec/IndexSpec.js
@@ -1,13 +1,16 @@
 describe('Index', () => {
+  let platformFactory;
   let gameFactory;
   let mockCanvas;
   let mockDocument;
   let mockContext;
+  let mockPlatform;
   let mockGame;
   let gameSize;
   let index;
 
   beforeEach(() => {
+    platformFactory = new PlatformFactory();
     gameFactory = new GameFactory();
 
     mockCanvas = {
@@ -19,9 +22,10 @@ describe('Index', () => {
     };
 
     mockContext = {};
-
+    mockPlatform = jasmine.createSpyObj('platform', ['draw']);
     mockGame = jasmine.createSpyObj('game', ['update', 'draw']);
 
+    spyOn(platformFactory, 'build').and.returnValue(mockPlatform);
     spyOn(gameFactory, 'build').and.returnValue(mockGame);
     spyOn(mockCanvas, 'getContext').and.returnValue(mockContext);
     spyOn(mockDocument, 'querySelector').and.returnValue(mockCanvas);
@@ -29,7 +33,7 @@ describe('Index', () => {
 
     gameSize = { x: mockCanvas.width, y: mockCanvas.height };
 
-    index = new Index(gameFactory, mockDocument);
+    index = new Index(gameFactory, platformFactory, mockDocument);
   });
 
   describe('tick', () => {
