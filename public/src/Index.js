879b45b8e4d1237c3017c90e2eb4c6925b2ac487
SiobhanGi <siobhan.goggins@gmail.com>
2018-07-03 16:25:05 +0100

    unjumpable platform added to index.js

--- public/src/Index.js
+++ public/src/Index.js
@@ -6,18 +6,23 @@ function Index(gameFactory = new GameFactory(), documentObject = document) {
     undefined,
     undefined,
     [
+
+      // meeting plaform
+      new Platform({ x: 800, y: 790 }, { x: 1600, y: -10 }, undefined, 'public/assets/imgs/platform_left_end.png'),
+
       // starting platform
       new Platform({ x: 800 - 128, y: 790 }, { x: 128, y: 128 }, undefined, 'public/assets/imgs/platform_left_end.png'),
       new Platform({ x: 800, y: 790 }, { x: 128, y: 128 }, undefined, 'public/assets/imgs/platform_mid.png'),
       new Platform({ x: 800 + 128, y: 790 }, { x: 128, y: 128 }, undefined, 'public/assets/imgs/platform_right_end.png'),
 
       // 1st right platform
-      new Platform({ x: 400, y: 550 }, { x: 128, y: 90 }, undefined, 'public/assets/imgs/platform_mid.png'),
+      new Platform({ x: 400, y: 550 }, { x: 70, y: 70 }, undefined, 'public/assets/imgs/platform_mid.png'),
 
 
-      // new Platform({ x: canvas.width / 4 + 128, y: 400 }, { x: 128, y: 64 }, undefined, 'public/assets/imgs/platform_right_end.png'),
-      //
-      // new Platform({ x: canvas.width * 3 / 4 -128, y: 400 }, { x: 200, y: 64 }, undefined, 'public/assets/imgs/platform_left_end.png'),
+      // 1st unjumpable platform - medium
+      new Platform({ x: 1330, y: 512 }, { x: 128, y: 90 }, undefined, 'public/assets/imgs/platform_left_end.png'),
+      new Platform({ x: 1330 + 128, y: 512 }, { x: 128, y: 90 }, undefined, 'public/assets/imgs/platform_right_end.png'),
+
       // new Platform({ x: canvas.width * 3 / 4, y: 400 }, { x: 200, y: 64 }, undefined, 'public/assets/imgs/platform_right_end.png')
     ]
   );
